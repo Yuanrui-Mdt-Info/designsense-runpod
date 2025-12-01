ac48aaf325ca904094eeeb4ca618008a7ae1cecb
Jackie.mba <yangjialong@nekteck.com>
2025-12-01 16:11:06 +0800

    add diffusers params

--- qwen_v2509/rp_handler.py
+++ qwen_v2509/rp_handler.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import runpod
 # from diffusers import DiffusionPipeline
 from diffusers import QwenImageEditPipeline
@@ -26,6 +29,11 @@ def handler(event):
         prompt = input_data.get("prompt", "Enhance the image")
         image_url = input_data.get("image_url")
         image_base64 = input_data.get("image_base64")
+        
+        true_cfg_scale = input_data.get("true_cfg_scale", 6.0)
+        num_inference_steps = input_data.get("num_inference_steps", 50)
+        negative_prompt = input_data.get("negative_prompt", "blurry, low quality, distorted")
+        seed = input_data.get("seed", None)
 
         # 支持 URL 或 Base64 输入
         if image_url:
@@ -37,8 +45,20 @@ def handler(event):
             input_image = Image.open(BytesIO(image_bytes)).convert("RGB")
         else:
             return {"error": "Missing 'image_url' or 'image_base64' parameter."}
+        
+        # 设置随机种子
+        generator = torch.manual_seed(seed) if seed is not None else None
 
-        output_image = pipe(image=input_image, prompt=prompt).images[0]
+        output_image = pipe(
+            image=input_image,
+            prompt=prompt,
+            generator=generator,
+            true_cfg_scale=true_cfg_scale,
+            num_inference_steps=num_inference_steps,
+            negative_prompt=negative_prompt,
+            guidance_scale=1.0,
+            num_images_per_prompt=1,
+        ).images[0]
 
         buffered = BytesIO()
         output_image.save(buffered, format="PNG")
@@ -47,6 +67,7 @@ def handler(event):
         return {"output_image_base64": img_str, "prompt": prompt}
     except Exception as e:
         return {"error": str(e)}
+
 
 # Required by Runpod
 runpod.serverless.start({"handler": handler})
